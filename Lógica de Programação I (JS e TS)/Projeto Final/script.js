b666e6cb2554409fda6c0431fa70f61affd8b0d4
DaniMagrini <danielle.magrini@hotmail.com>
2023-08-11 21:58:29 -0300

    tentando arrumar o modal do edit e ainda falta o pesquisar

--- Lógica de Programação I (JS e TS)/Projeto Final/script.js
+++ Lógica de Programação I (JS e TS)/Projeto Final/script.js
@@ -3,11 +3,16 @@ const addTask = document.querySelector("#addTask");
 const addInput = document.querySelector("#addInput");
 const todoList = document.querySelector("#to-do-list");
 const editItem = document.querySelector("#editItem");
+const modalContainerEdit = document.querySelector("#modalContainerEdit");
+const editInput = document.querySelector("#editInput");
+const saveButton = document.querySelector("#saveButton");
+
+let oldTaskTitle;
+
 
 //FUNÇÕES
-const savetask = (text) => {
-
-    //Criando a div de classe to-do-list-itens
+function insertTask(text) {
+    
     const task = document.createElement("div");
     task.classList.add("to-do-list-itens"); // adicionando uma classe a div
 
@@ -20,15 +25,14 @@ const savetask = (text) => {
     const checkBtn = document.createElement("button");
     checkBtn.classList.add("to-do-list-buttons", "checkItem", "to-do-list-buttonsItemCompleted");
     checkBtn.classList.add("checkItem"); // adicionando uma classe nesse botão
-    checkBtn.innerHTML = '<i class="fa-solid fa-clipboard-check fa-xl" style="color: #6bf50f;"></i>'; // colocando o icone dentro
+    checkBtn.innerHTML = '<i class="fa-solid fa-clipboard-check fa-xl checkItem" style="color: #6bf50f;"></i>'; // colocando o icone dentro
     task.appendChild(checkBtn); // colocando o botão dentro da div task
 
     
     //Criando os botões da div de classe to-do-list-itens (EDIT)
     const editBtn = document.createElement("button");
     editBtn.classList.add("to-do-list-buttons", "editItem", "to-do-list-buttonsItemCompleted");
-    editBtn.classList.add("editItem"); // adicionando uma classe nesse botão
-    editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square fa-xl" style="color: #fdf50d;"></i>'; // colocando o icone dentro
+    editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square fa-xl editItem" style="color: #fdf50d;"></i>'; // colocando o icone dentro
     task.appendChild(editBtn); // colocando o botão dentro da div task
 
     
@@ -36,7 +40,7 @@ const savetask = (text) => {
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("to-do-list-buttons", "deleteItem", "to-do-list-buttonsItemCompleted");
     deleteBtn.classList.add("deleteItem"); // adicionando uma classe nesse botão
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash fa-xl" style="color: #d82222;"></i>'; // colocando o icone dentro
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash fa-xl deleteItem" style="color: #d82222;"></i>'; // colocando o icone dentro
     task.appendChild(deleteBtn); // colocando o botão dentro da div task
 
     todoList.appendChild(task); // colocar na div geral
@@ -45,6 +49,23 @@ const savetask = (text) => {
     addInput.focus();
 }
 
+function openModal(modal) {
+    let divElement = document.getElementById(modal);
+    divElement.classList.add("active");
+    console.log(modal) //
+    //modal.style.display = flex; 
+}
+
+function closeModal(modal) {
+    console.log("fechou") 
+}
+
+function editTask() {
+
+    editItem.addEventListener('click', openModal('modalContainerEdit'));
+}
+
+
 //EVENTOS
 addTask.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -52,7 +73,7 @@ addTask.addEventListener("submit", (e) => {
     const inputValue = addInput.value;
 
     if (inputValue) {
-        savetask(inputValue)
+        insertTask(inputValue)
     }
 });
 
@@ -60,13 +81,18 @@ document.addEventListener("click", (e) => {
 
     const clickedElement = e.target; //pega o elemento clicado
     const divElement = clickedElement.closest("div"); // Selecionando a div mais próxima do elemento clicado
+    let taskTitle;
+
+    if (divElement && divElement.querySelector("h3")) {
+        taskTitle = divElement.querySelector("h3").innerText;
+    }
 
     if (clickedElement.classList.contains("checkItem")) { // vendo se o elemento clicado é o botão check
         divElement.classList.toggle("ItemCompleted");  // Se ele não tem a classe ItemCompleted, ele coloca, se ele tem, ele tira (toggle = troca)       
     }
 
     if (clickedElement.classList.contains("editItem")) { // vendo se o elemento clicado é o botão edit
-        divElement.classList.toggle("ItemCompleted");  // Se ele não tem a classe ItemCompleted, ele coloca, se ele tem, ele tira (toggle = troca) 
+        //openModalEdit();  // Se ele não tem a classe ItemCompleted, ele coloca, se ele tem, ele tira (toggle = troca) 
     }
 
     if (clickedElement.classList.contains("deleteItem")) { // vendo se o elemento clicado é o botão delete
